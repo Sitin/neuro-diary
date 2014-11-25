592d06d67674f9e332705edd8917ba5726bf8d9d
Mikhail Zyatin <mikhail.zyatin@gmail.com>
2014-11-25 19:55:03 +0200

    Several graphs

--- sample.py
+++ sample.py
@@ -8,14 +8,17 @@ import os
 
 __dir__ = os.path.dirname(os.path.realpath(__file__))
 
-df = neurod.parse(fixtures.path_to_sample_log_file).interpolate(method='slinear')
+df_all = neurod.parse(fixtures.path_to_sample_log_file).interpolate(method='slinear')
+df_att = neurod.parse(fixtures.path_to_sample_log_file, {'EEGATT'})
+df_med = neurod.parse(fixtures.path_to_sample_log_file, {'EEGMED'})
 
 # Write to CSV file
-df.to_csv(__dir__ + '/output/sample.csv')
+df_all.to_csv(__dir__ + '/output/sample.csv')
 
 # Plot data
-plt.figure()
-df.plot(x='TS')
+df_all.plot(x='TS', title='Attention & Meditation')
+df_att.plot(x='TS', title='Attention')
+df_med.plot(x='TS', title='Meditation')
 plt.show()
 
 
